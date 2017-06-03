fb2dbb13197f483652f81d9f510ebff475f671a9
guillain <guillain@gmail.com>
2017-06-04 01:32:13 +0200

    App init

--- SoI4IoT/__init__.py
+++ SoI4IoT/__init__.py
@@ -18,63 +18,27 @@ app.config.from_object(__name__)
 app.config.from_envvar('FLASK_SETTING')
 
 # Import SoI4IoT features
-from static.py.SoI4IoT import soi4ioT_api
-app.register_blueprint(soi4ioT_api)
+from static.py.login import login_api
+app.register_blueprint(login_api)
+
+from static.py.device import device_api
+app.register_blueprint(device_api)
+
+from static.py.user import user_api
+app.register_blueprint(user_api)
+
+from static.py.tracking import tracking_api
+app.register_blueprint(tracking_api)
+
+from static.py.dashboard import dashboard_api
+app.register_blueprint(dashboard_api)
 
 # WEB mgt ----------------------------------------
 @app.route('/')
 def my_form():
   if 'login' in session:
-    return render_template('new.html')
+    return render_template('newDevice.html')
   return render_template("login.html")
-
-# Login --------------------------------------------
-@app.route('/login', methods=['POST'])
-def login():
-    error = None
-    if 'login' in session:
-        return render_template('welcome.html')
-
-    login = request.form['login']
-    if not login:
-        wEvent('/login','arg','Thanks to provide login','KO')
-        return render_template('login.html')
-
-    password = request.form['password']
-    if not password:
-        wEvent('/login','arg','Thanks to provide password','KO')
-        return render_template('login.html')
-
-    try:
-        data = exeReq("SELECT grp, admin FROM iot WHERE login='"+login+"' AND password=PASSWORD('"+password+"')")
-    except Exception as e:
-        wEvent('/login','','DB connection/request', 'KO')
-        return render_template('login.html')
-
-    try:
-      if data is None or data[0][0] is None:
-        wEvent('/login','','Wrong email or password','KO')
-        return render_template('login.html')
-    except Exception as e:
-      wEvent('/login','','Wrong email or password','KO')
-      return render_template('login.html')
-
-    try:
-        session['login'] = str(login)
-        session['grp'] = str(data[0][0])
-        session['admin'] = str(data[0][1])
-        wEvent('/login',session['login'],"User "+session['login']+" logged",'OK')
-        return render_template('welcome.html')
-    except Exception as e:
-        wEvent('/login','','Wrong email or password','KO')
-        return render_template('login.html')
-
-# Logout --------------------------------------------------
-@app.route('/logout')
-def logout():
-  wEvent('/logout',session['login'],'You were logged out','OK')
-  session.clear()
-  return redirect('/')
 
 # End of App --------------------------------------------------------------------------
 if __name__ == '__main__':
