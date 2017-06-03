48d5a621c4f2a41caf121b873b770dfbd3ce0535
guillain <guillain@gmail.com>
2017-06-04 01:32:12 +0200

    MySQL structure

--- conf/mysql.sql
+++ conf/mysql.sql
@@ -1,54 +1,18 @@
---
--- Table structure for table `iot`
---
-DROP TABLE IF EXISTS `iot`;
-CREATE TABLE `iot` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(64) NOT NULL,
-  `firstname` varchar(64) NOT NULL,
-  `lastname` varchar(64) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `address` varchar(256) NOT NULL,
-  `enterprise` varchar(128) NOT NULL,
-  `grp` varchar(32) NOT NULL,
-  `mobile` varchar(16) NOT NULL,
-  `password` text NOT NULL,
-  `admin` boolean NOT NULL,
-  `ip` varchar(32) NOT NULL,
-  `url` varchar(128) NOT NULL,
-  `website` varchar(128) NOT NULL,
-  `webhook` varchar(128) NOT NULL,
-  `gps` varchar(32) NOT NULL,
-  `deviceid` int(11) NOT NULL,
-  `devicename` varchar(64) NOT NULL,
-  `devicedescription` varchar(256) NOT NULL,
-  `devicestatus` varchar(32) DEFAULT NULL,
-  `deviceupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `login` (`login`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `tracking`
---
-DROP TABLE IF EXISTS `tracking`;
-CREATE TABLE `tracking` (
-  `tid` int(11) NOT NULL AUTO_INCREMENT,
+-- Table structure for table `device`
+DROP TABLE IF EXISTS `device`;
+CREATE TABLE `device` (
+  `did` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
-  `ip` varchar(32) NOT NULL,
-  `url` varchar(128) NOT NULL,
-  `website` varchar(128) NOT NULL,
-  `webhook` varchar(128) NOT NULL,
-  `gps` varchar(32) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`tid`)
+  `name` varchar(64) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `status` varchar(32) DEFAULT NULL,
+  `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`did`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
 -- Table structure for table `events`
---
--- DROP TABLE IF EXISTS `events`;
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
   `eid` int(11) NOT NULL AUTO_INCREMENT,
   `module` varchar(32) NOT NULL,
@@ -59,4 +23,36 @@ CREATE TABLE `events` (
   PRIMARY KEY (`eid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Table structure for table `tracking`
+DROP TABLE IF EXISTS `tracking`;
+CREATE TABLE `tracking` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `did` int(11) NOT NULL,
+  `ip` varchar(32) NOT NULL,
+  `gps` varchar(32) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `website` varchar(128) NOT NULL,
+  `webhook` varchar(128) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Table structure for table `user`
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(64) NOT NULL,
+  `firstname` varchar(64) NOT NULL,
+  `lastname` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `enterprise` varchar(128) NOT NULL,
+  `grp` varchar(32) NOT NULL,
+  `mobile` varchar(16) NOT NULL,
+  `password` text NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
