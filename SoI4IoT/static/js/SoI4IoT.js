1f06a08cc2d900fd635de4c2f4a89218d6aa84c2
guillain <guillain@gmail.com>
2017-06-04 01:32:13 +0200

    jQuery of SoI4IoT

--- SoI4IoT/static/js/SoI4IoT.js
+++ SoI4IoT/static/js/SoI4IoT.js
@@ -6,11 +6,12 @@
  Copyright 2017 GPL - Guillain
 ***/
 
-/* newSub click */
+/* USER */
+/* newUserSub click */
   $(function() {
-    $('a#newSub').bind('click', function() {
+    $('a#newUserSub').bind('click', function() {
         $.ajax({
-            url: 'newSub',
+            url: 'newUserSub',
             data: $('form').serialize(),
             type: 'POST',
             success: function(data) {
@@ -23,11 +24,11 @@
     });
   });
 
-/* update click */
+/* updateUser click */
   $(function() {
-    $('a#update').bind('click', function() {
+    $('a#updateUser').bind('click', function() {
         $.ajax({
-            url: 'update',
+            url: 'updateUser',
             data: $('form').serialize(),
             type: 'POST',
             success: function(data) {
@@ -40,6 +41,77 @@
     });
   });
 
+/* DEVICE */
+/* newDeviceSub click */
+  $(function() {
+    $('a#newDeviceSub').bind('click', function() {
+        $.ajax({
+            url: 'newDeviceSub',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(data) {
+                $("#result").text(data);
+            },
+            error: function(error) {
+                $("#result").text(error);
+            }
+        });
+    });
+  });
+
+/* updateDevice click */
+  $(function() {
+    $('a#updateDevice').bind('click', function() {
+        $.ajax({
+            url: 'updateDevice',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(data) {
+                $("#result").text(data);
+            },
+            error: function(error) {
+                $("#result").text(error);
+            }
+        });
+    });
+  });
+
+/* TRACKING */
+/* newTrackingSub click */
+  $(function() {
+    $('a#newTrackingSub').bind('click', function() {
+        $.ajax({
+            url: 'newTrackingSub',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(data) {
+                $("#result").text(data);
+            },
+            error: function(error) {
+                $("#result").text(error);
+            }
+        });
+    });
+  });
+
+/* updateTracking click */
+  $(function() {
+    $('a#updateTracking').bind('click', function() {
+        $.ajax({
+            url: 'updateTracking',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(data) {
+                $("#result").text(data);
+            },
+            error: function(error) {
+                $("#result").text(error);
+            }
+        });
+    });
+  });
+
+/* ADD FEATURES */
 /* display additionnal info when onmouse */
 function onmouseoveragent(el) {
     var hint = el.querySelector("div.hideme");
@@ -52,3 +124,41 @@ function onmouseoutagent(el) {
     var hint = el.querySelector("div.hideme");
     hint.style.display = 'none';
 };
+
+/* Body load function */
+function bodyOnload(){
+}
+
+/* Get GPS position */
+function sendHTMLgps(){
+    var x = document.getElementById("gps");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(recPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function recPosition(position) {
+    var x = document.getElementById("gps");
+    var longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+    var loginurl = document.getElementById("login") + '';
+    var sURLVariables = loginurl.split('=');
+    var login = sURLVariables[1];
+    alert("Position has been recorded for the user " + login + "\n* Lat.: " + latitude + "\n* Long.: " + longitude);
+
+    $.ajax({
+            url: 'recGPS',
+            data: { 'login':login, 'latitude':latitude, 'longitude':longitude},
+            type: 'POST',
+            success: function(data) {
+                $("#result").text(data);
+            },
+            error: function(error) {
+                $("#result").text(error);
+            }
+        });
+
+}
+
+
