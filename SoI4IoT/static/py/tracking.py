a1a35a69b4f35dbd116e81f13decfa29e393d690
guillain <guillain@gmail.com>
2017-06-04 15:40:36 +0200

    Tracking script

--- SoI4IoT/static/py/tracking.py
+++ SoI4IoT/static/py/tracking.py
@@ -7,7 +7,7 @@
 from flask import Flask, session, redirect, url_for, escape, request
 from flask import render_template, jsonify, send_file
 from werkzeug.utils import secure_filename
-from tools import logger, exeReq, wEvent
+from tools import logger, exeReq, wEvent, getMaps
 
 import re, os, sys, urllib
 
@@ -19,51 +19,17 @@ api = Flask(__name__)
 api.config.from_object(__name__)
 api.config.from_envvar('FLASK_SETTING')
 
-
-# Record ------------------------------------------------------
-@tracking_api.route('/recGPS', methods=['POST', 'GET'])
-def recGPS():
-    gps = request.form['latitude'] + ',' + request.form['longitude'];
-
-    # Check if webbrowser device is recorded, if not add it
-    try:
-        sql  = "INSERT INTO device SET uid = (SELECT uid FROM user WHERE login = '" + request.form['login'] + "'), "
-        sql += "  name = '" + request.user_agent.browser + "', status = 'ok', description = '" + request.user_agent.string + "' "
-        sql += "ON DUPLICATE KEY UPDATE status = 'ok';"
-        exeReq(sql)
-
-        sql  = "SELECT did FROM device "
-        sql += "WHERE name = '" + request.user_agent.browser + "' AND status = 'ok' AND description = '" + request.user_agent.string + "' "
-        sql += "ORDER BY did DESC LIMIT 1;"
-        did = exeReq(sql)
-        wEvent('/recGPS','exeReq','Add or update web device','OK')
-    except Exception as e:
-        wEvent('/recGPS','exeReq','Add or update web device','KO')
-        return 'Add or update web device error'
-
-    # Add new localisation
-    try:
-        sql  = "INSERT INTO tracking SET "
-        sql += "  uid = (SELECT uid FROM user WHERE login = '" + request.form['login'] + "'), "
-        sql += "  did = '" + str(did[0][0]) + "', gps = '" + str(gps) + "';"
-        exeReq(sql)
-        wEvent('/recGPS','exeReq','GPS record','OK')
-        return 'GPS record OK'
-    except Exception as e:
-        wEvent('/recGPS','exeReq','GPS record','KO')
-        return 'GPS record error'
-
 # Tracking creation form -------------------------------------------
 @tracking_api.route('/newTracking', methods=['POST', 'GET'])
 def newTracking():
     wEvent('/newTracking','request','Get new tracking','')
-    return render_template('tracking.html')
+    return render_template('tracking.html', maps = '')
 
 # Save Tracking ---------------------------------------------------
 @tracking_api.route('/saveTracking', methods=['POST'])
 def saveTracking():
     try:
-        sql  = "INSERT INTO tracking SET "
+        sql  = "INSERT INTO tracking SET tid = '" + request.form['tracking'] + "', "
         sql += "  uid = (SELECT uid FROM user WHERE login = '" + request.form['login'] + "'), "
         sql += "  did = (SELECT did FROM device WHERE name = '" + request.form['name'] + "'), "
         sql += "  gps = '" + request.form['gps'] + "', url = '" +request.form['url'] + "', "
@@ -75,7 +41,6 @@ def saveTracking():
         sql += "  gps = '" + request.form['gps'] + "', url = '" +request.form['url'] + "', "
         sql += "  website = '" + request.form['website'] + "', webhook = '" + request.form['webhook'] + "', "
         sql += "  address = '" + request.form['address'] + "', ip = '" +request.form['ip'] + "';"
-        print sql
         exeReq(sql)
         wEvent('/saveTracking','exeReq','Save','OK')
         return 'Save OK'
@@ -92,7 +57,7 @@ def viewTracking():
         sql += "WHERE u.uid = t.uid AND t.tid = '" + request.args['tracking'] + "';"
         view = exeReq(sql)
         wEvent('/viewTracking','exeReq','Get','OK')
-        return render_template('tracking.html', view = view[0])
+        return render_template('tracking.html', view = view[0], maps = getMaps())
     except Exception as e:
         wEvent('/viewTracking','exeReq','Get','KO')
         return 'View error'
@@ -106,19 +71,7 @@ def listTracking():
         sql += "WHERE t.uid = u.uid AND t.did = d.did;"
         list = exeReq(sql)
         wEvent('/listTracking','exeReq','Get list','OK')
-        return render_template('listTracking.html', list = list)
+        return render_template('listTracking.html', list = list, maps = getMaps())
     except Exception as e:
         wEvent('/listTracking','exeReq','Get list','KO')
         return 'List error'
-
-# Map ----------------------------------------------------------
-@tracking_api.route('/mapTracking', methods=['GET', 'POST'])
-def mapTracking():
-    try:
-        data = exeReq("SELECT gps FROM tracking")
-        wEvent('/mapTracking','exeReq','Get','OK')
-        return render_template('gmap.html', gmap = data)
-    except Exception as e:
-        wEvent('/mapTracking','exeReq','Get','KO')
-        return 'Map error'
-
