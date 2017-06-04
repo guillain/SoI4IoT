e0bf77cceca8ea9c280d2c82a2304acf668570ee
guillain <guillain@gmail.com>
2017-06-04 15:40:36 +0200

    Tools library as DB connection, wEvent...

--- SoI4IoT/static/py/tools.py
+++ SoI4IoT/static/py/tools.py
@@ -4,19 +4,41 @@
 # Mail: guillain@gmail.com
 # Copyright 2017 GPL - Guillain
 
-from flask import Flask
-from flask import flash
+from flask import Flask, session, redirect, url_for, escape, request, flash
 import MySQLdb
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASK_SETTING')
 
+# GPS SQL fct
+def getMaps():
+    # User arg as SQL condition
+    try:
+        login = " AND t.uid = u.uid AND u.login = '" + request.args['login'] + "'"
+    except Exception as e:
+        login = ' AND t.uid = u.uid'
+
+    # Device arg as SQL condition
+    try:
+        name = " AND t.did = d.did AND d.name = '" + request.args['name'] + "'"
+    except Exception as e:
+        name = ' AND t.did = d.did'
+
+    # Tracking arg as SQL condition
+    try:
+        tracking = " AND t.tid = '" + request.args['tracking'] + "'"
+    except Exception as e:
+        tracking = ''
+
+    return exeReq("SELECT t.gps FROM tracking t, user u, device d WHERE 1" + login + name + tracking + ";")
+
 # FUNCTIONs ---------------------------
 # Log function (push in flash + log)
 def logger(fct,msg):
   flash(msg)
-  print(str(fct+": "+msg))
+  if app.config['DEBUG'] == 'True':
+    print(str(fct+": "+msg))
   return
 
 def wEvent(module, user, msg, status = None):
