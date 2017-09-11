b2c80ae661eba9ac79f845476fda19b8cae4c4d1
guillain <guillain@gmail.com>
2017-09-11 06:41:13 +0200

    Tracking script, 1.3.0, Ful REST API Ok for GET & DELETE

--- SoI4IoT/static/py/tracking.py
+++ SoI4IoT/static/py/tracking.py
@@ -20,13 +20,13 @@ app.config.from_object(__name__)
 app.config.from_envvar('FLASK_SETTING')
 
 # Tracking creation form -------------------------------------------
-@tracking_app.route('/newTracking', methods=['POST', 'GET'])
+@tracking_app.route('/html/v1.0/tracking/new', methods=['POST', 'GET'])
 def newTracking():
-    wEvent('/newTracking','request','Get new tracking','')
+    wEvent('/html/v1.0/tracking/new','request','Get new tracking','')
     return render_template('tracking.html', maps = '', loginList = loginList(), nameList = nameList())
 
 # Save Tracking ---------------------------------------------------
-@tracking_app.route('/saveTracking', methods=['POST'])
+@tracking_app.route('/html/v1.0/tracking/save', methods=['POST'])
 def saveTracking():
     try:
         sql  = "INSERT INTO tracking SET tid = '" + request.form['tracking'] + "', "
@@ -46,36 +46,36 @@ def saveTracking():
         sql += "  humidity = '" + request.form['humidity'] + "', luminosity = '" + request.form['luminosity'] + "', "
         sql += "  temp_amb = '" + request.form['temp_amb'] + "', temp_sensor = '" +request.form['temp_sensor'] + "';"
         exeReq(sql)
-        wEvent('/saveTracking','exeReq','Save','OK')
+        wEvent('/html/v1.0/tracking/save','exeReq','Save','OK')
         return 'Save OK'
     except Exception as e:
-        wEvent('/saveTracking','exeReq','Save','KO')
+        wEvent('/html/v1.0/tracking/save','exeReq','Save','KO')
         return 'Save error'
 
 # View Tracking ---------------------------------------------------
-@tracking_app.route('/viewTracking', methods=['POST', 'GET'])
+@tracking_app.route('/html/v1.0/tracking/view', methods=['POST', 'GET'])
 def viewTracking():
     try:
         sql  = "SELECT t.tid, u.login, d.name, t.ip, t.gps, t.url, t.website, t.webhook, t.address, t.timestamp, t.humidity, t.luminosity, t.temp_amb, t.temp_sensor "
         sql += "FROM tracking t, user u, device d "
         sql += "WHERE u.uid = t.uid AND t.did = d.did AND t.tid = '" + request.args['tracking'] + "';"
         view = exeReq(sql)
-        wEvent('/viewTracking','exeReq','Get','OK')
+        wEvent('/html/v1.0/tracking/view','exeReq','Get','OK')
         return render_template('tracking.html', view = view[0], maps = getMaps(), loginList = loginList(), nameList = nameList())
     except Exception as e:
-        wEvent('/viewTracking','exeReq','Get','KO')
+        wEvent('/html/v1.0/tracking/view','exeReq','Get','KO')
         return 'View error'
 
 # List Tracking --------------------------------------------------------
-@tracking_app.route('/listTracking', methods=['POST', 'GET'])
+@tracking_app.route('/html/v1.0/tracking/list', methods=['POST', 'GET'])
 def listTracking():
     try:
         sql  = "SELECT t.tid, u.login, d.name, t.timestamp, CONCAT_WS(t.ip, t.gps, t.url, t.website, t.webhook, t.address, t.humidity, t.luminosity, t.temp_amb, t.temp_sensor) "
         sql += "FROM tracking t, user u, device d "
         sql += "WHERE t.uid = u.uid AND t.did = d.did AND u.grp != 'deleted' AND d.status != 'deleted';"
         list = exeReq(sql)
-        wEvent('/listTracking','exeReq','Get list','OK')
+        wEvent('/html/v1.0/tracking/list','exeReq','Get list','OK')
         return render_template('listTracking.html', list = list, maps = getMaps())
     except Exception as e:
-        wEvent('/listTracking','exeReq','Get list','KO')
+        wEvent('/html/v1.0/tracking/list','exeReq','Get list','KO')
         return 'List error'
